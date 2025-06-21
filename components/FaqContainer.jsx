070a0bf256ac86a7695066482cd06aec3b6fc919
luan-services <luandotservices@gmail.com>
2025-06-21 02:22:58 -0300

    fixing

--- components/FaqContainer.jsx
+++ components/FaqContainer.jsx
@@ -5,7 +5,7 @@ const FaqContainer = () => {
     return (
         
         <div className="relative w-full flex flex-col min-h-screen items-center px-6">
-            <div className="absolute top-0 w-full h-48 bg-center bg-no-repeat bg-[url('../src/assets/images/background-pattern-desktop.svg')]">
+            <div className="absolute top-0 w-full h-48 bg-center bg-repeat bg-[url('../public/background-pattern-desktop.svg')]">
             </div>
             <FaqSection/>
         </div>
