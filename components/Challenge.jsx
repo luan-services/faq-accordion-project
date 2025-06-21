6c775ba12787d5261b7f1a0f1f5566210e6f6826
luan-services <luandotservices@gmail.com>
2025-06-21 02:29:33 -0300

    fixed

--- components/Challenge.jsx
+++ components/Challenge.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Challenge = () => {
   return (
-        <div className="w-full flex justify-end py-2 px-4 gap-4 text-custom-white">
+        <div className="w-full flex justify-end py-2 px-4 gap-4">
             <div className="flex text-sm">
                 <span className=""> 
                     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
--- components/FaqContainer.jsx
+++ components/FaqContainer.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import FaqSection from "./FaqSection"
+import bgImg from "../src/assets/images/background-pattern-desktop.svg"
 
 const FaqContainer = () => {
     return (
         
         <div className="relative w-full flex flex-col min-h-screen items-center px-6">
-            <div className="absolute top-0 w-full h-48 bg-center bg-repeat bg-[url('../public/background-pattern-desktop.svg')]">
+            <div className="absolute top-0 w-full">
+                <img src={bgImg} className="bg-center bg-repeat w-full h-48 object-cover"/>
             </div>
             <FaqSection/>
         </div>
